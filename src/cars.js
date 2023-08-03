35f85c1d7093d2f6a2e6c45f1e4233cca243ad29
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-08-03 15:28:47 +0800

    React exam: added cars

--- src/cars.js
+++ src/cars.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 export default function Cars(){
+    const carArray = ["Ford", "BMW", "Audi"];
+
     return(
-        <>
-        </>
+        <div className="cars">
+            <h1>Who lives in my garage?</h1>
+            <ul>
+                {carArray.map((car) => {return <li>I am a { car }</li>})}
+            </ul>
+        </div>
     )
 }
